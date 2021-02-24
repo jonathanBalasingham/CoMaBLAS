f3499d8a657a44c4cd59be2dfb6689f388065901
jonathanBalasingham <jonbalasin@gmail.com>
2021-02-24 21:11:26 +0000

    vector scaling implementation for s,d,c,z

--- blas/coma_scal.c
+++ blas/coma_scal.c
@@ -4,10 +4,101 @@
 
 #include "coma_scal.h"
 
-float sscal_float(int n, float sa, float *sx, int incx) {
-    return 0;
+void sscal(unsigned int n, float sa, float *sx, int incx) {
+    int m, nincx;
+
+    if (n == 0)
+        return;
+
+    if (incx == 1) {
+        m = n % 5;
+        if (m != 0){
+            for (int j = 0; j < m; ++j) {
+                sx[j] = sx[j] * sa;
+            }
+
+            if (n < 5)
+                return;
+        }
+
+        for (int j = m; j < n; j += 5) {
+            sx[j] = sx[j] * sa;
+            sx[j+1] = sx[j+1] * sa;
+            sx[j+2] = sx[j+2] * sa;
+            sx[j+3] = sx[j+3] * sa;
+            sx[j+4] = sx[j+4] * sa;
+        }
+
+    } else {
+        nincx = n * incx;
+        for (int i = 0; i < nincx; i += incx) {
+            sx[i] = sx[i] * sa;
+        }
+    }
+
 }
 
-double sscal_double(int n, double sa, double *sx, int incx) {
-    return 0;
+void dscal(unsigned int n, double sa, double *sx, int incx) {
+    int m, nincx;
+
+    if (n == 0)
+        return;
+
+    if (incx == 1) {
+        m = n % 5;
+        if (m != 0){
+            for (int j = 0; j < m; ++j) {
+                sx[j] = sx[j] * sa;
+            }
+
+            if (n < 5)
+                return;
+        }
+
+        for (int j = m; j < n; j += 5) {
+            sx[j] = sx[j] * sa;
+            sx[j+1] = sx[j+1] * sa;
+            sx[j+2] = sx[j+2] * sa;
+            sx[j+3] = sx[j+3] * sa;
+            sx[j+4] = sx[j+4] * sa;
+        }
+
+    } else {
+        nincx = n * incx;
+        for (int i = 0; i < nincx; i += incx) {
+            sx[i] = sx[i] * sa;
+        }
+    }
+}
+
+void cscal(int n, complex float sa, complex float *sx, int incx) {
+    if (n == 0)
+        return;
+
+    if (incx == 1){
+        for (int i = 0; i < n; ++i) {
+            sx[i] = sx[i] * sa;
+        }
+    } else {
+        int nincx = n * incx;
+        for (int i = 0; i < nincx; i += incx) {
+            sx[i] = sx[i] * sa;
+        }
+    }
+}
+
+void zscal(int n, complex double sa, complex double *sx, int incx) {
+    if (n == 0)
+        return;
+
+    if (incx == 1){
+        for (int i = 0; i < n; ++i) {
+            sx[i] = sx[i] * sa;
+        }
+    } else {
+        int nincx = n * incx;
+        for (int i = 0; i < nincx; i += incx) {
+            sx[i] = sx[i] * sa;
+        }
+    }
 }
--- blas/coma_scal.h
+++ blas/coma_scal.h
@@ -5,7 +5,13 @@
 #ifndef COMABLAS_COMA_SCAL_H
 #define COMABLAS_COMA_SCAL_H
 
-float sscal_float(int n, float sa, float* sx, int incx);
-double sscal_double(int n, double sa, double* sx, int incx);
+#include <complex.h>
+
+void sscal(unsigned int n, float sa, float* sx, int incx);
+void dscal(unsigned int n, double sa, double* sx, int incx);
+void cscal(int n, complex float sa,complex float* sx, int incx);
+void zscal(int n,complex double sa,complex double* sx, int incx);
+
+
 
 #endif //COMABLAS_COMA_SCAL_H
