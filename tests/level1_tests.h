3ebd8fdcd47ab44e58a193960767a5a7ffb7ba0f
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-05 12:22:58 +0000

    fix test suite memory error

--- tests/level1_tests.h
+++ tests/level1_tests.h
@@ -58,13 +58,13 @@ MunitResult ddot_over5(const MunitParameter params[], void* user_data_or_fixture
 MunitResult test_snrm2(const MunitParameter params[], void* user_data_or_fixture) {
     unsigned int n = 3;
     float t1[3] = {1,2,3};
-    munit_assert_float(3.74165, ==, snrm2(n, (float *) &t1, 1));
+    munit_assert_double_equal(3.741657, snrm2(n, (float *) &t1, 1), 6);
 }
 
 // TODO: What precision?
 MunitResult test_dnrm2(const MunitParameter params[], void* user_data_or_fixture) {
     unsigned int n = 3;
-    float t1[3] = {1,2,3};
+    double t1[3] = {1,2,3};
     munit_assert_double_equal(3.7416573867739, dnrm2(n, (double *) &t1, 1), 10);
 }
 
@@ -78,7 +78,7 @@ MunitResult test_scnrm2(const MunitParameter params[], void* user_data_or_fixtur
 MunitResult test_dznrm2(const MunitParameter params[], void* user_data_or_fixture) {
     unsigned int n = 3;
     complex float t1[3] = {1 + 1*I, 2 + 2*I, 3 + 3*I};
-    assert_double_equal((double) 2*sqrt(7), scnrm2(n, (complex float *) &t1, 1), 10);
+    assert_double_equal((double) 2*sqrt(7), scnrm2(n, (complex float *) &t1, 1), 5);
 }
 
 MunitResult test_scopy(const MunitParameter params[], void* user_data_or_fixture) {
@@ -112,8 +112,8 @@ MunitResult test_ccopy(const MunitParameter params[], void* user_data_or_fixture
     unsigned int n = 6;
     complex float t1[6] = {1,2,3,4,5,6};
     complex float* t2;
-    t2 = malloc(sizeof(t1));
-    scopy(n, &t1, 1, (float *) t2, 1);
+    t2 = malloc(n*sizeof(t1[0]));
+    ccopy(n, (const complex float *) &t1, 1, t2, 1);
     for (int i = 0; i < n; ++i) {
         assert_float(t1[i], ==, t2[i]);
     }
@@ -158,29 +158,29 @@ MunitResult test_dscal(const MunitParameter params[], void* user_data_or_fixture
     }
 }
 
-// TODO: adjust assertion
 MunitResult test_cscal(const MunitParameter params[], void* user_data_or_fixture) {
     unsigned int n = 6;
     complex float t1[6] = {1+1*I,2+2*I,3+3*I,4+4*I,5+5*I,6+6*I};
-    const complex float t2[6] = {2+2*I,4+4*I,6+6*I,8+8*I,10+10*I,12+12*I};
+    const complex float t2[6] = {1+1*I,2+2*I,3+3*I,4+4*I,5+5*I,6+6*I};
     complex float sa = 2 + 1*I;
     cscal(n, sa, (complex float *) &t1, 1);
 
     for (int i = 0; i < n; ++i) {
-        assert_float(t1[i], ==, t2[i]);
+        assert_float(crealf(t1[i]), ==, crealf(sa) * crealf(t2[i]) - cimagf(sa) * cimagf(t2[i]));
+        assert_float(cimagf(t1[i]), ==, crealf(sa) * cimagf(t2[i]) + cimagf(sa) * crealf(t2[i]));
     }
 }
 
-// TODO: adjust assertion
 MunitResult test_zscal(const MunitParameter params[], void* user_data_or_fixture) {
     unsigned int n = 6;
     complex double t1[6] = {1+1*I,2+2*I,3+3*I,4+4*I,5+5*I,6+6*I};
-    const complex double t2[6] = {2+2*I,4+4*I,6+6*I,8+8*I,10+10*I,12+12*I};
+    const complex double t2[6] = {1+1*I,2+2*I,3+3*I,4+4*I,5+5*I,6+6*I};
     complex double sa = 2 + 1*I;
-    cscal(n, sa, (complex float *) &t1, 1);
+    zscal(n, sa, (complex double *) &t1, 1);
 
     for (int i = 0; i < n; ++i) {
-        assert_double(t1[i], ==, t2[i]);
+        assert_double(creal(t1[i]), ==, creal(sa) * creal(t2[i]) - cimag(sa) * cimag(t2[i]));
+        assert_double(cimag(t1[i]), ==, creal(sa) * cimag(t2[i]) + cimag(sa) * creal(t2[i]));
     }
 }
 
@@ -194,7 +194,7 @@ MunitResult test_saxpy(const MunitParameter params[], void* user_data_or_fixture
     saxpy(n, sa, (float *) &t1, 1, (float *) &t2, 1);
 
     for (int i = 0; i < n; ++i) {
-        assert_float(t1[i], ==, t3[i]);
+        assert_float(t2[i], ==, t3[i]);
     }
 }
 
@@ -202,27 +202,140 @@ MunitResult test_daxpy(const MunitParameter params[], void* user_data_or_fixture
     unsigned int n = 6;
     double t1[6] = {1,2,3,4,5,6};
     double t2[6] = {2,4,6,8,10,12};
-    float t3[6] = {4,8,12,16,20,24};
+    double t3[6] = {4,8,12,16,20,24};
 
     double sa = 2;
     daxpy(n, sa, (double *) &t1, 1, (double *) &t2, 1);
 
     for (int i = 0; i < n; ++i) {
-        assert_double(t1[i], ==, t3[i]);
+        assert_double(t2[i], ==, t3[i]);
     }
 }
 
-typedef MunitResult (*munit_test)(const MunitParameter[], void*);
 
-static const munit_test level1_tests[16] = {&sdot_under5, &sdot_over5, &ddot_under5, &ddot_under5,
+static const MunitTest level1_tests[] = {
+        {
+                (char*) "/dot/sdot1",
+                sdot_over5,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/dot/sdot2",
+                sdot_under5,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/dot/ddot1",
+                ddot_over5,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/dot/ddot2",
+                ddot_under5,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/nrm/snrm2",
+                test_snrm2,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/nrm/dnrm2",
+                test_dnrm2,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/nrm/scnrm2",
+                test_scnrm2,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/nrm/dznrm2",
+                test_dznrm2,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/copy/scopy",
+                test_scopy,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/copy/dcopy",
+                test_dcopy,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/copy/ccopy",
+                test_ccopy,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/copy/zcopy",
+                test_zcopy,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/scal/sscal",
+                test_sscal,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/scal/dscal",
+                test_dscal,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/scal/cscal",
+                test_cscal,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/scal/zscal",
+                test_zscal,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/axpy/saxpy",
+                test_saxpy,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        {
+                (char*) "/axpy/daxpy",
+                test_daxpy,
+                MUNIT_TEST_OPTION_NONE,
+                NULL
+        },
+        { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
+
+/*
+        { &sdot_under5, &sdot_over5, &ddot_under5, &ddot_under5,
                                            &test_snrm2, &test_scnrm2, &test_dnrm2, &test_dznrm2,
                                            &test_scopy, &test_dcopy, &test_ccopy, &test_zcopy,
                                            &test_sscal, &test_dscal, &test_cscal, &test_zscal,
                                            &test_saxpy, &test_daxpy};
+*/
 
 static const MunitSuite suite = {
-        "/my-tests", /* name */
-        &level1_tests, /* tests */
+        "/level1", /* name */
+        level1_tests, /* tests */
         NULL, /* suites */
         1, /* iterations */
         MUNIT_SUITE_OPTION_NONE /* options */
--- tests/main.c
+++ tests/main.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include "level1_tests.h"
 
-int main (int argc, const char* argv[]) {
+#include "../munit/munit.h"
+
+int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
     return munit_suite_main(&suite, NULL, argc, argv);
 }
