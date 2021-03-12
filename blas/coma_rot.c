6ccb13941551956a1a3f7459d16b057cdf45d16d
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-12 15:39:12 +0000

    csrot & zdrotf implementation

--- blas/coma_rot.c
+++ blas/coma_rot.c
@@ -62,3 +62,61 @@ void drot(unsigned int n, double *x, int incx, double *y, int incy, double c, do
         }
     }
 }
+
+void csrot(unsigned int n, complex float *x, int incx, complex float *y, int incy, float c, float s) {
+    complex float temp;
+
+    if (n == 0)
+        return;
+
+    if (incx == 1 && incy == 1) {
+        for (int i = 0; i < n; ++i) {
+            temp = c*x[i] + s * y[i];
+            y[i] = c * y[i] - s * x[i];
+            x[i] = temp;
+        }
+    } else {
+        int ix = 0, iy = 0;
+        if (incx < 0)
+            ix = (-1*n+1) * incx + 1;
+        if (incy < 0)
+            iy = (-1*n+1) * incy + 1;
+
+        for (int i = 0; i < n; ++i) {
+            temp = c * x[ix] + s * y[iy];
+            y[iy] = c * y[iy] - s * x[ix];
+            x[ix] = temp;
+            ix += incx;
+            iy += incy;
+        }
+    }
+}
+
+void zdrotf(unsigned int n, complex double *x, int incx, complex double *y, int incy, double c, double s) {
+    complex double temp;
+
+    if (n == 0)
+        return;
+
+    if (incx == 1 && incy == 1) {
+        for (int i = 0; i < n; ++i) {
+            temp = c*x[i] + s * y[i];
+            y[i] = c * y[i] - s * x[i];
+            x[i] = temp;
+        }
+    } else {
+        int ix = 0, iy = 0;
+        if (incx < 0)
+            ix = (-1*n+1) * incx + 1;
+        if (incy < 0)
+            iy = (-1*n+1) * incy + 1;
+
+        for (int i = 0; i < n; ++i) {
+            temp = c * x[ix] + s * y[iy];
+            y[iy] = c * y[iy] - s * x[ix];
+            x[ix] = temp;
+            ix += incx;
+            iy += incy;
+        }
+    }
+}
--- blas/coma_rot.h
+++ blas/coma_rot.h
@@ -4,9 +4,12 @@
 
 #ifndef COMABLAS_COMA_ROT_H
 #define COMABLAS_COMA_ROT_H
+#include <complex.h>
 
 void srot(unsigned int n, float* sx, int incx, float* sy, int incy, float c, float s);
 void drot(unsigned int n, double* sx, int incx, double* sy, int incy, double c, double s);
+void csrot(unsigned int n,complex float* sx, int incx,complex float* sy, int incy, float c, float s);
+void zdrotf(unsigned int n,complex double* sx, int incx,complex double* sy, int incy, double c, double s);
 
 
 #endif //COMABLAS_COMA_ROT_H
