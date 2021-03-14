cf9a11160bff2ea1aa751f57ed6a286353928002
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-14 12:36:56 +0000

    remove unsigned ints

--- blas/coma_gemv.h
+++ blas/coma_gemv.h
@@ -6,10 +6,12 @@
 #define COMABLAS_COMA_GEMV_H
 #include <complex.h>
 
-void sgemv(char trans, unsigned int m, unsigned int n, float alpha, float** A, int lda, float* x, unsigned int incx, float beta, float* y, unsigned int incy);
-void dgemv(char trans, unsigned int m, unsigned int n, double alpha, double** A, int lda, double* x, unsigned int incx, double beta, double* y, unsigned int incy);
-void cgemv(char trans, unsigned int m, unsigned int n, complex float alpha, complex float** A, int lda, complex float* x, unsigned int incx, complex float beta, complex float* y, unsigned int incy);
-void zgemv(char trans, unsigned int m, unsigned int n, complex double alpha,complex double** A, int lda,complex double* x, unsigned int incx,complex double beta,complex double* y, unsigned int incy);
+void sgemv(char trans, int m, int n, float alpha, float** A, int lda, float* x, int incx, float beta, float* y, int incy);
+void dgemv(char trans, int m, int n, double alpha, double** A, int lda, double* x, int incx, double beta, double* y, int incy);
+void cgemv(char trans, int m, int n, complex float alpha, complex float** A, int lda,
+           complex float* x, int incx, complex float beta, complex float* y, int incy);
+void zgemv(char trans, int m, int n, complex double alpha,complex double** A, int lda,
+           complex double* x, int incx,complex double beta,complex double* y, int incy);
 
 
 
