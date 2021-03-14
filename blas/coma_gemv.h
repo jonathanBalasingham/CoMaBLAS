2bdd803a8d05cf8ab90ba924342ff951f7bbf2e5
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-14 12:28:14 +0000

    prototype matrix-vector mult

--- blas/coma_gemv.h
+++ blas/coma_gemv.h
@@ -6,10 +6,10 @@
 #define COMABLAS_COMA_GEMV_H
 #include <complex.h>
 
-void sgemv(char trans, unsigned int m, unsigned int n, float alpha, float** A, int LDA, float* x, unsigned int incx, float beta, float* y, unsigned int incy);
-void dgemv(char trans, unsigned int m, unsigned int n, double alpha, double** A, int LDA, double* x, unsigned int incx, double beta, double* y, unsigned int incy);
-void cgemv(char trans, unsigned int m, unsigned int n, complex float alpha, complex float** A, int LDA, complex float* x, unsigned int incx, complex float beta, complex float* y, unsigned int incy);
-void zgemv(char trans, unsigned int m, unsigned int n, complex double alpha,complex double** A, int LDA,complex double* x, unsigned int incx,complex double beta,complex double* y, unsigned int incy);
+void sgemv(char trans, unsigned int m, unsigned int n, float alpha, float** A, int lda, float* x, unsigned int incx, float beta, float* y, unsigned int incy);
+void dgemv(char trans, unsigned int m, unsigned int n, double alpha, double** A, int lda, double* x, unsigned int incx, double beta, double* y, unsigned int incy);
+void cgemv(char trans, unsigned int m, unsigned int n, complex float alpha, complex float** A, int lda, complex float* x, unsigned int incx, complex float beta, complex float* y, unsigned int incy);
+void zgemv(char trans, unsigned int m, unsigned int n, complex double alpha,complex double** A, int lda,complex double* x, unsigned int incx,complex double beta,complex double* y, unsigned int incy);
 
 
 
