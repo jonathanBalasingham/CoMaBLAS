72dbe9977aec0114e7cb3fd2ca200732de6366a2
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-18 23:20:51 +0000

    cgbmv implementation

--- blas/coma_gbmv.c
+++ blas/coma_gbmv.c
@@ -293,3 +293,150 @@ dgbmv(char trans, int m, int n, int kl, int ku, double alpha, double **A, int ld
         }
     }
 }
+
+void cgbmv(char trans, int m, int n, int kl, int ku, complex float alpha, complex float **A, int lda, complex float *x,
+           int incx, complex float beta, complex float *y, int incy) {
+    int info = _validate_gbmv_inputs((char)toupper(trans), m,n,kl,ku,lda,incx,incy);
+
+    if (info != 0){
+        // error
+        return;
+    }
+
+    if (m == 0 || n == 0 || (alpha == 0 && beta == 1))
+        return;
+
+
+    int noconj = (char) toupper(trans) == 'T';
+    int lenx, leny, kx, ky;
+
+    if (trans == 'N') {
+        lenx = n;
+        leny = m;
+    } else {
+        lenx = m;
+        leny = n;
+    }
+
+    if (incx > 0)
+        kx = 0;
+    else
+        kx = 1 - (lenx - 1) * incx;
+
+    if (incy > 0)
+        ky = 0;
+    else
+        ky = 1 - (leny - 1) * incy;
+
+
+
+    if (beta != 1) {
+        if (incy == 1) {
+            if (beta == 0) {
+                for (int i = 0; i < leny; ++i) {
+                    y[i] = 0;
+                }
+            } else {
+                for (int i = 0; i < leny; ++i) {
+                    y[i] = beta * y[i];
+                }
+            }
+        } else {
+            int iy = ky;
+            if (beta == 0) {
+                for (int i = 0; i < leny; ++i) {
+                    y[iy] = 0;
+                    iy += incy;
+                }
+            } else {
+                for (int i = 0; i < leny; ++i) {
+                    y[iy] = beta * y[iy];
+                    iy += incy;
+                }
+            }
+        }
+    }
+
+    if (alpha == 0)
+        return;
+
+    int kup1 = ku + 1;
+    double temp;
+    int k, start, stop;
+    if (trans == 'N') {
+        int jx = kx;
+        if (incy == 1) {
+            for (int i = 0; i < n; ++i) {
+                temp = alpha * x[jx];
+                k = kup1 - i;
+                start = 0 > i - ku ? 0 : i - ku;
+                stop = m < i + kl ? m : i + kl;
+                for (int j = start; j < stop; ++j) {
+                    y[j] += temp * A[k+j][i];
+                }
+                jx += incx;
+            }
+        } else {
+            int iy;
+            for (int i = 0; i < n; ++i) {
+                temp = alpha * x[jx];
+                iy = ky;
+                k = kup1 - i;
+                start = 0 > i - ku ? 0 : i - ku;
+                stop = m < i + kl ? m : i + kl;
+                for (int j = start; j < stop; ++j) {
+                    y[iy] += temp * A[k+j][i];
+                    iy += incy;
+                }
+                jx += incx;
+                if (i > ku)
+                    ky += incy;
+            }
+        }
+    } else {
+        int jy = ky;
+        if (incx == 1) {
+            for (int i = 0; i < n; ++i) {
+                temp = 0;
+                k = kup1 - i;
+                start = 0 > i - ku ? 0 : i - ku;
+                stop = m < i + kl ? m : i + kl;
+                if (noconj) {
+                    for (int j = start; j < stop; ++j) {
+                        temp += A[k+j][i]*x[j];
+                    }
+                } else {
+                    for (int j = start; j < stop; ++j) {
+                        temp += conjf(A[k+j][i])*x[j];
+                    }
+                }
+                y[jy] += alpha * temp;
+                jy += incy;
+            }
+        } else {
+            int ix;
+            for (int i = 0; i < n; ++i) {
+                temp = 0;
+                ix = kx;
+                k = kup1 - i;
+                start = 0 > i - ku ? 0 : i - ku;
+                stop = m < i + kl ? m : i + kl;
+                if (noconj) {
+                    for (int j = start; j < stop; ++j) {
+                        temp += A[k+j][i] * x[ix];
+                        ix += incx;
+                    }
+                } else {
+                    for (int j = start; j < stop; ++j) {
+                        temp += conjf(A[k+j][i]) * x[ix];
+                        ix += incx;
+                    }
+                }
+                y[jy] += alpha * temp;
+                jy += incy;
+                if (i > ku)
+                    kx += incx;
+            }
+        }
+    }
+}
