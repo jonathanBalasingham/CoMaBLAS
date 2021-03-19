ea6959420b5e094096c7c9cb41757fdfd18ee39c
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-19 22:42:46 +0000

    symmetric matvec

---
+++ blas/coma_symv.h
@@ -0,0 +1,11 @@
+//
+// Created by jon on 3/19/21.
+//
+
+#ifndef COMABLAS_COMA_SYMV_H
+#define COMABLAS_COMA_SYMV_H
+
+void ssymv(char uplo, int n, float alpha, float** A, int lda, int incx, float beta, float* y, int incy);
+void dsymv(char uplo, int n, double alpha, double** A, int lda, int incx, double beta, double* y, int incy);
+
+#endif //COMABLAS_COMA_SYMV_H
