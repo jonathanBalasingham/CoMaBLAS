1e97ab556de7e66c25b39710f5179760c3628468
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-20 18:28:27 +0000

    fix symv prototypes

--- blas/coma_symv.h
+++ blas/coma_symv.h
@@ -5,7 +5,7 @@
 #ifndef COMABLAS_COMA_SYMV_H
 #define COMABLAS_COMA_SYMV_H
 
-void ssymv(char uplo, int n, float alpha, float** A, int lda, int incx, float beta, float* y, int incy);
-void dsymv(char uplo, int n, double alpha, double** A, int lda, int incx, double beta, double* y, int incy);
+void ssymv(char uplo, int n, float alpha, float** A, int lda, float* x, int incx, float beta, float* y, int incy);
+void dsymv(char uplo, int n, double alpha, double** A, int lda, double* x, int incx, double beta, double* y, int incy);
 
 #endif //COMABLAS_COMA_SYMV_H
