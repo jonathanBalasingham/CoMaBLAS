fe9a743f3ed8570e07fdaf249f7715710943895f
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-24 22:34:33 +0000

    spmv prototypes

---
+++ blas/coma_spmv.c
@@ -0,0 +1,10 @@
+
+#include "coma_spmv.h"
+
+void sspmv(char uplo, int n, float alpha, float **AP, float *x, int incx, float beta, float *y, int incy) {
+
+}
+
+void dspmv(char uplo, int n, double alpha, double **AP, double *x, int incx, double beta, double *y, int incy) {
+
+}
