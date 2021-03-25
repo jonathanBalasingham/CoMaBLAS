551bd63462a0f9efedbba10c0701107f07bacb46
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-25 16:35:26 +0000

    spmv prototype updates

--- blas/coma_spmv.h
+++ blas/coma_spmv.h
@@ -1,8 +1,8 @@
 #ifndef COMABLAS_COMA_SPMV_H
 #define COMABLAS_COMA_SPMV_H
 
-void sspmv(char uplo, int n, float alpha, float** AP, float* x, int incx, float beta, float* y, int incy);
-void dspmv(char uplo, int n, double alpha, double** AP, double* x, int incx, double beta, double* y, int incy);
+void sspmv(char uplo, int n, float alpha, float* AP, float* x, int incx, float beta, float* y, int incy);
+void dspmv(char uplo, int n, double alpha, double* AP, double* x, int incx, double beta, double* y, int incy);
 
 
 #endif //COMABLAS_COMA_SPMV_H
