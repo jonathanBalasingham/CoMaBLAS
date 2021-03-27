bbe84118ed9cfa6d993e093b148616cd2e2066d2
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-27 13:48:04 +0000

    update library

--- library.c
+++ library.c
@@ -1,6 +1,7 @@
 #include "library.h"
 
 #include <stdio.h>
+#include "blas/coma_dot.h"
 
 void hello(void) {
     printf("Hello, World!\n");
--- library.h
+++ library.h
@@ -1,6 +1,11 @@
 #ifndef COMABLAS_LIBRARY_H
 #define COMABLAS_LIBRARY_H
 
+#include "./blas/coma_dot.h"
+#include "./blas/coma_asum.h"
+#include "./blas/coma_axpy.h"
+#include "./blas/coma_copy.h"
+
 void hello(void);
 
 #endif //COMABLAS_LIBRARY_H
