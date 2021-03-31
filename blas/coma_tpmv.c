7b53889981c2096453fbebc3433a79b1dbdf41d3
jonathanBalasingham <jonbalasin@gmail.com>
2021-03-31 20:17:03 +0100

    tpmv scaffolding

---
+++ blas/coma_tpmv.c
@@ -0,0 +1,21 @@
+//
+// Created by jon on 3/31/21.
+//
+
+#include "coma_tpmv.h"
+
+void stpmv(char uplo, char trans, char diag, int n, int k, float **A, float *x, int incx) {
+
+}
+
+void dtpmv(char uplo, char trans, char diag, int n, int k, double **A, double *x, int incx) {
+
+}
+
+void ctpmv(char uplo, char trans, char diag, int n, int k, complex float **A, complex float *x, int incx) {
+
+}
+
+void ztpmv(char uplo, char trans, char diag, int n, int k, complex double **A, complex double *x, int incx) {
+
+}
