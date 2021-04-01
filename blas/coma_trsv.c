517ffad236163c19fb03b607ee6016c99964613e
jonathanBalasingham <jonbalasin@gmail.com>
2021-04-01 22:47:39 +0100

    trsv scaffolding

---
+++ blas/coma_trsv.c
@@ -0,0 +1,21 @@
+//
+// Created by jon on 4/1/21.
+//
+
+#include "coma_trsv.h"
+
+void strsv(char uplo, char trans, char diag, int n, float **A, int lda, float *x, int incx) {
+
+}
+
+void dtrsv(char uplo, char trans, char diag, int n, double **A, int lda, double *x, int incx) {
+
+}
+
+void ctrsv(char uplo, char trans, char diag, int n, complex float **A, int lda, complex float *x, int incx) {
+
+}
+
+void ztrsv(char uplo, char trans, char diag, int n, complex double **A, int lda, complex double *x, int incx) {
+
+}
